012d85ab365317dfd93facad73b66cfc991bc1c8
usama <usamanadeem1991@gmail.com>
2018-01-29 16:02:54 +0500

    added crud methods in DBAdapter

--- app/src/main/java/com/example/usama/popularmovies/utils/DBAdapter.java
+++ app/src/main/java/com/example/usama/popularmovies/utils/DBAdapter.java
@@ -1,9 +1,15 @@
 package com.example.usama.popularmovies.utils;
 
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.usama.popularmovies.model.Movie;
+
+import java.util.ArrayList;
 
 public class DBAdapter {
 
@@ -45,6 +51,70 @@ public class DBAdapter {
         }
         return dbAdapterInstance;
     }
+
+    public void addMovie(Movie movie) {
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_MOVIE_ID, movie.getMovieId());
+        values.put(COLUMN_MOVIE_NAME, movie.getMovieTitle());
+        values.put(COLUMN_USER_RATING, movie.getMovieVoteAverage());
+        values.put(COLUMN_RELEASE_DATE, movie.getMovieReleaseDate());
+        values.put(COLUMN_OVERVIEW, movie.getPlotSynopsis());
+        values.put(COLUMN_LOCAL_POSTER_PATH, movie.getMoviePosterPath());
+
+        //SQLiteDatabase database = this.getWritableDatabase();
+        sqLiteDatabase.insert(TABLE_MOVIES, null, values);
+        sqLiteDatabase.close();
+    }
+
+    public void deleteMovie(Movie movie) {
+        //String query = "DELETE FROM movies WHERE movieid = " + movie.getMovieId();
+        // sqLiteDatabase.execSQL(query);
+        sqLiteDatabase.delete(TABLE_MOVIES,
+                COLUMN_MOVIE_ID + " = " + movie.getMovieId(),
+                null);
+        sqLiteDatabase.close();
+    }
+
+    public boolean checkIfRowExists(Movie movie) {
+
+        //SQLiteDatabase database = this.getReadableDatabase();
+        String query = "Select * from " + TABLE_MOVIES + " where " + COLUMN_MOVIE_ID + " = " + movie.getMovieId();
+        Cursor cursor = sqLiteDatabase.rawQuery(query, null);
+        if (cursor.getCount() <= 0) {
+            cursor.close();
+            sqLiteDatabase.close();
+            return false;
+        }
+        cursor.close();
+        sqLiteDatabase.close();
+        return true;
+
+    }
+
+    public ArrayList<Movie> getFavouriteMovies() {
+        ArrayList<Movie> movies = new ArrayList<>();
+
+        Cursor cursor = sqLiteDatabase.rawQuery("select * from " + TABLE_MOVIES, null);
+        if (cursor.moveToFirst()) {
+            while (!cursor.isAfterLast()) {
+                String movieId = cursor.getString(cursor.getColumnIndex(COLUMN_MOVIE_ID));
+                String movieName = cursor.getString(cursor.getColumnIndex(COLUMN_MOVIE_NAME));
+                String userRating = cursor.getString(cursor.getColumnIndex(COLUMN_USER_RATING));
+                String releaseDate = cursor.getString(cursor.getColumnIndex(COLUMN_RELEASE_DATE));
+                String overview = cursor.getString(cursor.getColumnIndex(COLUMN_OVERVIEW));
+                String posterPath = cursor.getString(cursor.getColumnIndex(COLUMN_LOCAL_POSTER_PATH));
+
+                Movie movie = new Movie(movieId, movieName, releaseDate, posterPath, userRating, overview);
+
+                movies.add(movie);
+                cursor.moveToNext();
+            }
+            cursor.close();
+            sqLiteDatabase.close();
+        }
+        return movies;
+    }
+
 
     private static class DBHelper extends SQLiteOpenHelper {
 
